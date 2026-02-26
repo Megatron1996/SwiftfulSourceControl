82df95c5051c9d0eddbab8f2ecbbe4133e9852b4
Zohidjon Aktamov <45931021+Megatron1996@users.noreply.github.com>
2026-02-26 11:25:41 +0500

    in second view added zstak on it

---
+++ SwiftfulSourceControl/SeconView.swift
@@ -0,0 +1,21 @@
+//
+//  SeconView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Zohidjon on 26/02/26.
+//
+
+import SwiftUI
+
+struct SeconView: View {
+    var body: some View {
+
+        ZStack{
+            Text("my text of coding")
+        }
+    }
+}
+
+#Preview {
+    SeconView()
+}
