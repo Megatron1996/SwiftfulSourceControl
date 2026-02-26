e157cba1c11e47b6c80cb1d41f9d7835042b4a8c
Zohidjon Aktamov <45931021+Megatron1996@users.noreply.github.com>
2026-02-26 09:18:48 +0500

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Zohidjon on 26/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
